db02e31c2c4a3b253a8a9abc44d0df42c57a8261
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2017-10-01 17:58:30 +0200

    [plugins] move deploy to root

--- notamagic-analytics/notamagic_google_analytics.php
+++ notamagic-analytics/notamagic_google_analytics.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Notamagic: Google Analytics Plugin
 Plugin URI: http://notamagic.com
-Description: Adds a Google analytics trascking code to the <head> of your theme, by hooking to wp_head.
+Description: Adds a Google analytics tracking code to the <head> of your theme, by hooking to wp_head.
 Author: Dragan Filipovic
 Version: 1.0
  */
